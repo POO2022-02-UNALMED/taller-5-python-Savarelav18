c5e739f71e984cd796130327dc87939154104d45
Savarelav18 <111089079+Savarelav18@users.noreply.github.com>
2022-09-26 17:59:28 -0500

    actualizacion get y set de clase pez
    actualizacion get y set de clase pez debido a mal nombramiento de los parametros

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -36,11 +36,11 @@ class Pez(Animal):
     
     def getColorEscamas(self):
         return self._colorEscamas
-    def getColorEscamas(self,colorEscamas):
+    def setColorEscamas(self,colorEscamas):
         self._colorEscamas=colorEscamas
     
-    def getColorEscamas(self):
+    def getCantidadAletas(self):
         return self._cantidadAletas
-    def getColorEscamas(self,cantidadAletas):
+    def setCantidadAletas(self,cantidadAletas):
         self._cantidadAletas=cantidadAletas
     
